965d82501b0e4e4c3ffad748736959ef36f3d8d2
MartinHage <hagemartins@gmail.com>
2022-04-01 09:04:13 +0200

    upgrade to react 18

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.scss";
 import "./normalize.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
